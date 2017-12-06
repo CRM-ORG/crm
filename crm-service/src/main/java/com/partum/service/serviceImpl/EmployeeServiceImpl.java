831d1171b6989268b77eb87a5f5b856c18414d27
payne <payne@qq.com>
2017-12-06 14:39:19 +0800

    shiro 加密方式

--- crm-service/src/main/java/com/partum/service/serviceImpl/EmployeeServiceImpl.java
+++ crm-service/src/main/java/com/partum/service/serviceImpl/EmployeeServiceImpl.java
@@ -15,15 +15,20 @@ public class EmployeeServiceImpl extends ServiceImpl<EmployeeDao, Employee> impl
 
     @Override
     public int insertEmployee(Employee employee) {
-
+        /**
+         * 选择加密方式为新增的员工密码进行加密
+         * 使用MD5方式并且迭代1024次进行加密
+         */
         String hashAlgorithmName = "MD5";
         Integer hashIterations = 1024;
         Object credentials = employee.getEmpPassword();
         Object salt = employee.getEmpName();
-
+        /**
+         * 使用shiro自带的加密方法
+         */
         Object result =  new SimpleHash(hashAlgorithmName, credentials, salt, hashIterations);
         System.out.println(result);
         employee.setEmpPassword((String) result);
-        return 0;
+        return employeeDao.insertEmployee(employee);
     }
 }
